dc0169c5a8c1dbb67d82604ae49b064eb277425b
Denis Makarenko <xhjkl@users.noreply.github.com>
2023-05-19 11:00:12 +0300

    `tests/max_commission_percentage` → `tests/validators_curation`

--- program/tests/tests/mod.rs
+++ program/tests/tests/mod.rs
@@ -8,7 +8,7 @@ pub mod change_reward_distribution;
 pub mod deposit;
 pub mod limits;
 pub mod maintainers;
-pub mod max_commission_percentage;
+pub mod validators_curation;
 pub mod merge_stake;
 pub mod solana_assumptions;
 pub mod stake_deposit;
