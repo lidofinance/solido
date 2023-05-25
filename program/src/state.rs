88ca70316f5cd5061875e1fa74dc8d4ca9d49937
Denis Makarenko <xhjkl@users.noreply.github.com>
2023-05-25 08:31:31 +0300

    `ValidatorPerf::LEN` → 48

--- program/src/state.rs
+++ program/src/state.rs
@@ -555,7 +555,7 @@ impl ValidatorPerf {}
 impl Sealed for ValidatorPerf {}
 
 impl Pack for ValidatorPerf {
-    const LEN: usize = 40;
+    const LEN: usize = 48;
     fn pack_into_slice(&self, data: &mut [u8]) {
         let mut data = data;
         BorshSerialize::serialize(&self, &mut data).unwrap();
